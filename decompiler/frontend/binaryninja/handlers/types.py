b7974ad57323075c8534f700ab435530d5d091c8
Mariia Rybalka <mariia.rybalka@fkie.fraunhofer.de>
2022-07-25 13:37:48 +0200

    Resort imports once again

--- decompiler/frontend/binaryninja/handlers/types.py
+++ decompiler/frontend/binaryninja/handlers/types.py
@@ -1,4 +1,4 @@
-from binaryninja.types import ArrayType, BoolType, CharType, FloatType, IntegerType, NamedTypeReferenceType, PointerType, Type, StructureType, VoidType
+from binaryninja.types import ArrayType, BoolType, CharType, FloatType, IntegerType, NamedTypeReferenceType, PointerType, StructureType, Type,  VoidType
 from decompiler.frontend.lifter import Handler
 from decompiler.structures.pseudo import CustomType, Float, Integer, Pointer, UnknownType
 
