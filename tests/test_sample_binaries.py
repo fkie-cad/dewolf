0c3bf63841f7c09890c8369e5b7fa4be6ba36672
Mariia Rybalka <mariia.rybalka@fkie.fraunhofer.de>
2022-11-14 14:57:40 +0100

    Consider not only crashes but also error in function body for systemtests

--- tests/test_sample_binaries.py
+++ tests/test_sample_binaries.py
@@ -7,7 +7,8 @@ import pytest
 def test_sample(test_cases):
     """Test the decompiler with the given test case."""
     sample, function_name = test_cases
-    subprocess.run(("python", "decompile.py", sample, function_name), check=True)
+    output = subprocess.run(("python", "decompile.py", sample, function_name), check=True, stdout=subprocess.PIPE)
+    assert "Failed to decompile due to error during " not in output.stdout.decode('utf-8')
 
 
 def test_globals():
