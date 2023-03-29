673ead5c70821123f82680f7dab53b137a13f981
Spartak Ehrlich <spartakehrlich@outlook.com>
2023-03-29 13:46:49 +0200

    More generic renaming + more docs

--- decompiler/pipeline/controlflowanalysis/variable_name_generation.py
+++ decompiler/pipeline/controlflowanalysis/variable_name_generation.py
@@ -1,4 +1,5 @@
 import re
+from abc import ABC, abstractmethod
 from enum import Enum
 from typing import List, Optional
 
@@ -10,13 +11,14 @@ from decompiler.task import DecompilerTask
 
 
 def _get_var_counter(var_name: str) -> Optional[str]:
+    """Return the counter of a given variable name, if any is present."""
     if counter := re.match(r".*?([0-9]+)$", var_name):
         return counter.group(1)
     return None
 
 
 def _get_containing_variables(dfo: DataflowObject) -> List[Variable]:
-    """Returns a list of variables contained in this dataflow object"""
+    """Returns a list of variables contained in this dataflow object."""
     variables: List[Variable] = []
     for sub_exp in dfo.subexpressions():
         if isinstance(sub_exp, Variable):
@@ -25,51 +27,34 @@ def _get_containing_variables(dfo: DataflowObject) -> List[Variable]:
 
 
 class NamingConvention(str, Enum):
+    """Enum for the currently available naming conventions."""
     default = "default"
     system_hungarian = "system_hungarian"
 
 
-class VariableNameGeneration(PipelineStage):
-    """ 
-    Pipelinestage in charge of renaming variables to a configured format.
-    Currently only the 'default' or 'hungarian' system are supported.
-    """
+class RenamingScheme(ABC):
+    """Base class for different Renaming schemes."""
 
-    name : str = "variable-name-generation"
-    type_prefix = {
-        Float: {16: "h", 32: "f", 64: "d", 80: "ld", 128: "q", 256: "o"},
-        Integer: {8: "ch", 16: "s", 32: "i", 64: "l", 128: "i128"},
-    }
-
-
-    def __init__(self):
-        self._ast: Optional[AbstractSyntaxTree] = None
-        self._notation: Optional[str] = None
-        self._var_name: Optional[str] = None
-        self._pointer_base: bool = True
-        self._type_separator: str = ""
-        self._counter_separator: str = ""
+    def __init__(self, task: DecompilerTask) -> None:
+        self._ast: AbstractSyntaxTree = task._ast
         self._variables: List[Variable] = []
-        self._params : List[Variable] = []
+        self._loop_vars : List[Variable] = []
+        self._params: List[Variable] = task._function_parameters
 
 
-    def run(self, task: DecompilerTask):
-        self._ast = task.syntax_tree
-        self._notation = task.options.getstring(f"{self.name}.notation", fallback="default")
-        self._var_name = task.options.getstring(f"{self.name}.variable_name", fallback="Var")
-        self._pointer_base = task.options.getboolean(f"{self.name}.pointer_base", fallback=True)
-        self._type_separator = task.options.getstring(f"{self.name}.type_separator", fallback="")
-        self._counter_separator = task.options.getstring(f"{self.name}.counter_separator", fallback="")
-        self._params = task.function_parameters
-
-        if self._notation != NamingConvention.default:
-            self._collect()
-            self._purge()
-            self._rename()
+    def _filter_variables(self, item: Variable) -> bool:
+        """Return False if variable is a parameter, renamed loop variable or GlobalVariable, else True"""
+        if item in self._params or (item in self._loop_vars and item.name.find("var_") == -1) or isinstance(item, GlobalVariable):
+            return False
+        return True
 
 
-    def _collect(self):
-        """Collects all variables by iterating over every node in the AST."""
+    def collectVariableNames(self):
+        """Collects all variables and loop variable by iterating over every node in the AST and removing ones which should not be renamed:
+            - fkt parameter
+            - loop variables which have been renamed by For/WhileLoopRenamer
+            - global variables
+        """
         for node in self._ast.topological_order():
             if isinstance(node, CodeNode):
                 for stmt in node.instructions:
@@ -78,36 +63,53 @@ class VariableNameGeneration(PipelineStage):
                 for expr in [self._ast.condition_map[symbol] for symbol in node.condition.get_symbols()]:
                     self._variables.extend(_get_containing_variables(expr))
             elif isinstance(node, (SwitchNode, CaseNode)):
-                self._variables.extend(_get_containing_variables(node.expression))         
-
-
-    def _purge(self):
-        """Remove variables for renaming if they are:
-            - function params 
-            - loop variables which have been renamed by For/WhileLoopRenamer
-            - global vars with a symbol as a name
-        """
-        loop_vars : List[Variable] = []
+                self._variables.extend(_get_containing_variables(node.expression))     
+        
         for node in self._ast.get_loop_nodes_post_order():
             for expr in [self._ast.condition_map[symbol] for symbol in node.condition.get_symbols()]:
-                loop_vars.extend(_get_containing_variables(expr))
+                self._loop_vars.extend(_get_containing_variables(expr))
             
-        loop_vars = [var for var in loop_vars if var.name.find("var_") == -1]
-        self._variables = [var for var in self._variables if var not in self._params]
-        self._variables = [var for var in self._variables if var not in loop_vars]
-        self._variables = [var for var in self._variables if not isinstance(var, GlobalVariable)]
+        self._variables = list(filter(self._filter_variables, self._variables))  
 
-    def _rename(self):
+
+    @abstractmethod
+    def renameVariableNames(self):
+        """Abstract method which should rename variables with respect to the used scheme."""
+        pass
+
+
+class HungarianScheme(RenamingScheme):
+    """Class which renames variables into hungarian notation."""
+
+    type_prefix = {
+        Float: {16: "h", 32: "f", 64: "d", 80: "ld", 128: "q", 256: "o"},
+        Integer: {8: "ch", 16: "s", 32: "i", 64: "l", 128: "i128"},
+    }
+
+
+    def __init__(self, task: DecompilerTask) -> None:
+        super().__init__(task)
+        self._name = VariableNameGeneration.name
+        self._var_name: str = task.options.getstring(f"{self._name}.variable_name", fallback="Var")
+        self._pointer_base: bool = task.options.getboolean(f"{self._name}.pointer_base", fallback=True)
+        self._type_separator: str = task.options.getstring(f"{self._name}.type_separator", fallback="")
+        self._counter_separator: str = task.options.getstring(f"{self._name}.counter_separator", fallback="")
+    
+
+    def renameVariableNames(self):
+        """Rename all collected variables to the hungarian notation."""
         for var in self._variables:
             counter = _get_var_counter(var.name)
             var._name = self._hungarian_notation(var, counter if counter else "")
             
 
     def _hungarian_notation(self, var: Variable, counter: int) -> str:
+        """Return hungarian notation to a given variable."""
         return f"{self._hungarian_prefix(var.type)}{self._type_separator}{self._var_name}{self._counter_separator}{counter}"
 
 
     def _hungarian_prefix(self, var_type: Type) -> str:
+        """Return hungarian prefix to a given variable type."""
         if isinstance(var_type, Pointer):
             if self._pointer_base:
                 return f"{self._hungarian_prefix(var_type.type)}p"
@@ -121,3 +123,45 @@ class VariableNameGeneration(PipelineStage):
             sign = "" if var_type.is_signed else "u"
             prefix = self.type_prefix[type(var_type)][var_type.size]
             return f"{sign}{prefix}"
+
+
+class DefaultScheme(RenamingScheme):
+    """Class which renames variables into the default scheme."""
+
+    def __init__(self, task: DecompilerTask) -> None:
+        super().__init__(task)
+
+
+    def renameVariableNames(self):
+        # Maybe make the suboptions more generic, so that the default scheme can also be changed by some parameters?
+        pass
+
+
+class VariableNameGeneration(PipelineStage):
+    """ 
+    Pipelinestage in charge of renaming variables to a configured format.
+    Currently only the 'default' or 'hungarian' system are supported.
+    """
+
+    name : str = "variable-name-generation"
+
+    def __init__(self):
+        self._notation: str = None
+
+
+    def run(self, task: DecompilerTask):
+        """Rename variable names to the given scheme."""
+        self._notation = task.options.getstring(f"{self.name}.notation", fallback="default")
+
+        renamer: RenamingScheme = None
+
+        match self._notation:
+            case NamingConvention.default:
+                renamer = DefaultScheme(task)
+            case NamingConvention.system_hungarian:
+                renamer = HungarianScheme(task)
+            case _:
+                return
+
+        renamer.collectVariableNames()
+        renamer.renameVariableNames()
