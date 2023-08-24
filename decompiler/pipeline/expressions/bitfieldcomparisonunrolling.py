39c03a8f482f6fd4c2720dee149c3d2e4c120b80
Marvin Marks <marvin.marks@posteo.de>
2023-08-24 20:14:30 +0200

    add x86 matching

--- decompiler/pipeline/expressions/bitfieldcomparisonunrolling.py
+++ decompiler/pipeline/expressions/bitfieldcomparisonunrolling.py
@@ -1,4 +1,5 @@
-from logging import warning
+from datetime import datetime
+from logging import debug, warning
 from typing import Any, List, Optional, Tuple
 
 from decompiler.pipeline.stage import PipelineStage
@@ -7,11 +8,9 @@ from decompiler.structures.graphs.branches import FalseCase, TrueCase, Unconditi
 from decompiler.structures.graphs.cfg import ControlFlowGraph
 from decompiler.structures.pseudo import Constant, Expression
 from decompiler.structures.pseudo.expressions import Variable
-from decompiler.structures.pseudo.instructions import Branch, Instruction
+from decompiler.structures.pseudo.instructions import Branch
 from decompiler.structures.pseudo.operations import BinaryOperation, Condition, OperationType, UnaryOperation
 from decompiler.task import DecompilerTask
-from decompiler.util.decoration import DecoratedCFG
-from datetime import datetime
 
 
 class BitFieldComparisonUnrolling(PipelineStage):
@@ -41,14 +40,43 @@ class BitFieldComparisonUnrolling(PipelineStage):
         worklist = []
         for block in task.graph:
             if (matched_instruction := self._unfold_cases(block)) is not None:
-                switch_var, cases = matched_instruction
-                worklist.append((block, switch_var, cases))
-        for todo in worklist: 
+                switch_var, cases, is_negated = matched_instruction
+                worklist.append((block, switch_var, cases, is_negated))
+        for todo in worklist:
             self._modify_cfg(task.graph, *todo)
 
-    def _modify_cfg(self, cfg: ControlFlowGraph, block: BasicBlock, switch_var: Variable, cases: List[int]):
+    def _modify_cfg(self, cfg: ControlFlowGraph, block: BasicBlock, switch_var: Variable, cases: List[int], is_negated_condition: bool):
         """
-        TODO: what about negated condition?
+        Create a nested if blocks for each case in unfolded values.
+        Note: with the Branch condition encountered so far (== 0x0), the node of the collected cases is adjacent to the FalseCase edge.
+        However, negated conditions may exist. In this case, pass condition type (flag) and swap successor nodes accordingly.
+        """
+        debug("modifying cfg")
+        true_edge, false_edge = self._tf_edges_from_block(cfg, block)
+        other_node, case_node = true_edge.sink, false_edge.sink
+        if is_negated_condition:
+            other_node, case_node = case_node, other_node
+        block.remove_instruction(block[-1])
+        cfg.remove_edge(true_edge)
+        cfg.remove_edge(false_edge)
+        nested_if_blocks = [self._create_condition_block(cfg, switch_var, case_value) for case_value in cases]
+        for pred, succ in zip(nested_if_blocks, nested_if_blocks[1:]):
+            cfg.add_edge(TrueCase(pred, case_node))
+            cfg.add_edge(FalseCase(pred, succ))
+        # add edges for last and first block
+        cfg.add_edge(TrueCase(nested_if_blocks[-1], case_node))
+        cfg.add_edge(FalseCase(nested_if_blocks[-1], other_node))
+        cfg.add_edge(UnconditionalEdge(block, nested_if_blocks[0]))
+
+    def _create_condition_block(self, cfg: ControlFlowGraph, switch_var: Any, case_value: int) -> BasicBlock:
+        """
+        Create conditional block in CFG, e.g., `if (var == 0x42)`.
+        """
+        return cfg.create_block([Branch(condition=Condition(OperationType.equal, [switch_var, Constant(case_value)]))])
+
+    def _tf_edges_from_block(self, cfg: ControlFlowGraph, block: BasicBlock) -> Tuple[TrueCase, FalseCase]:
+        """
+        Return TrueCase, FalseCase edges from conditional block (in that order).
         """
         match cfg.get_out_edges(block):
             case (TrueCase() as true_edge, FalseCase() as false_edge):
@@ -56,54 +84,37 @@ class BitFieldComparisonUnrolling(PipelineStage):
             case (FalseCase() as false_edge, TrueCase() as true_edge):
                 pass
             case _:
-                raise ValueError("Branch does not have corresponding T/F edges.")
-        # true_node, false_node = true_edge.sink, false_edge.sink
-        false_node, true_node  = true_edge.sink, false_edge.sink # mind****
-        block.remove_instruction(block[-1])
-        cfg.remove_edge(true_edge)
-        cfg.remove_edge(false_edge)
-        nested_if_blocks = []
-        for case_value in cases:
-            new_block = cfg.create_block([Branch(condition=Condition(OperationType.equal, [switch_var, Constant(case_value)]))])
-            nested_if_blocks.append(new_block)
-        for pred, succ in zip(nested_if_blocks, nested_if_blocks[1:]):
-            cfg.add_edge(TrueCase(pred, true_node))
-            cfg.add_edge(FalseCase(pred, succ))
-        cfg.add_edge(TrueCase(succ, true_node))
-        cfg.add_edge(FalseCase(succ, false_node))
-        cfg.add_edge(UnconditionalEdge(block, nested_if_blocks[0]))
-        DecoratedCFG.from_cfg(cfg).export_plot(f"modified{datetime.now()}.png")
+                raise ValueError("Block does not have outgoing T/F edges.")
+        return true_edge, false_edge
 
-    def _unfold_cases(self, block: BasicBlock) -> Optional[Tuple[Any, List[int]]]:
+    def _unfold_cases(self, block: BasicBlock) -> Optional[Tuple[Any, List[int], bool]]:
+        """
+        Unfold Branch condition (checking bit field) into switch variable and list of case values.
+        """
         if not len(block):
             return None
-        if (subexpr := self._get_subexpression_for_unrolling(block[-1])) is not None:
-            if (matched_expression := self._get_switch_var_and_bitfield(subexpr)) is not None:
-                switch_var, bit_field = matched_expression
-                cleaned_var = self._clean_variable(switch_var)
-                case_values = self._get_values(bit_field)
-                if cleaned_var and case_values:
-                    return cleaned_var, case_values
-        return None
-
-    def _clean_variable(self, expr: Expression) -> Optional[Variable]:
-        if isinstance(expr, Variable):
-            return expr
-        if isinstance(expr, UnaryOperation) and expr.operation == OperationType.cast:
-            if len(expr.requirements) == 1:
-                return expr.requirements[0]
-
-    def _get_subexpression_for_unrolling(self, instr: Instruction):
-        match instr:
-            case Branch(condition=Condition(OperationType.equal, subexpr, Constant(value=0x0))):
-                return subexpr
+        if not isinstance(branch_instruction := block[-1], Branch):
+            return None
+        match branch_instruction.condition:
+            case Condition(OperationType.equal, subexpr, Constant(value=0x0)):
+                is_negated = False
+            case Condition(OperationType.not_equal, subexpr, Constant(value=0x0)):
+                is_negated = True
             case _:
                 return None
+        if (matched_expression := self._get_switch_var_and_bitfield(subexpr)) is not None:
+            switch_var, bit_field = matched_expression
+            cleaned_var = self._clean_variable(switch_var)
+            case_values = self._get_values(bit_field)
+            if cleaned_var and case_values:
+                return cleaned_var, case_values, is_negated
+        return None
 
     def _get_switch_var_and_bitfield(self, subexpr: Expression) -> Optional[Tuple[Any, Constant]]:
         """
         Match expression of folded switch case:
-            ((1 << (cast)var) & 0xffffffff)) & bit_field_constant)
+            a) ((1 << (cast)var) & 0xffffffff) & bit_field_constant
+            b) (0x1 << ((1: ) ecx#1)) & 0xa50
         """
         match subexpr:
             case BinaryOperation(
@@ -112,10 +123,16 @@ class BitFieldComparisonUnrolling(PipelineStage):
                     OperationType.bitwise_and, BinaryOperation(OperationType.left_shift, Constant(value=1), switch_var), Constant()
                 ),
                 Constant() as bit_field,
-            ):
-                assert bit_field.value not in {0xFFFFFFFF, 0xFFFF}, "TODO"
+            ) if bit_field.value != 0xFFFFFFFF:
+                return switch_var, bit_field
+            case BinaryOperation(
+                OperationType.bitwise_and,
+                BinaryOperation(OperationType.left_shift, Constant(value=1), switch_var),
+                Constant() as bit_field,
+            ) if bit_field.value != 0xFFFFFFFF:
                 return switch_var, bit_field
             case _:
+                debug(f"no match for {subexpr}")
                 return None
 
     def _get_values(self, const: Constant) -> List[int]:
@@ -129,3 +146,13 @@ class BitFieldComparisonUnrolling(PipelineStage):
             if bit == "1":
                 values.append(pos)
         return values
+
+    def _clean_variable(self, expr: Expression) -> Optional[Variable]:
+        """
+        Remove cast from Variable.
+        """
+        if isinstance(expr, Variable):
+            return expr
+        if isinstance(expr, UnaryOperation) and expr.operation == OperationType.cast:
+            if len(expr.requirements) == 1:
+                return expr.requirements[0]
