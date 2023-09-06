db813cf5f444d65ae5092229367a33b4e17a04f3
rihi <19492038+rihi@users.noreply.github.com>
2023-09-06 16:27:42 +0200

    Add docstring to modification.py/normalize_int(int,int,bool)

--- decompiler/pipeline/controlflowanalysis/expression_simplification/modification.py
+++ decompiler/pipeline/controlflowanalysis/expression_simplification/modification.py
@@ -110,6 +110,18 @@ def _constant_fold_shift(constants: list[Constant], fun: Callable[[int, int, int
 
 
 def normalize_int(v: int, size: int, signed: bool) -> int:
+    """
+    Normalizes an integer value to a specific size and signedness.
+
+    This function takes an integer value 'v' and normalizes it to fit within
+    the specified 'size' in bits by discarding overflowing bits. If 'signed' is
+    true, the value is treated as a signed integer.
+
+    :param v: The value to be normalized.
+    :param size: The desired bit size for the normalized integer.
+    :param signed: True if the integer should be treated as signed.
+    :return: The normalized integer value.
+    """
     value = v & ((1 << size) - 1)
     if signed and value & (1 << (size - 1)):
         return value - (1 << size)
