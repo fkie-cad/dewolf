892c4fc5155aeacc2e2630ebe3fef642518d610e
Rihi <19492038+rihi@users.noreply.github.com>
2023-09-20 15:41:43 +0200

    Update documentation for 'normalize_int'
    Co-authored-by: Manuel Blatt <45859907+blattm@users.noreply.github.com>

--- decompiler/pipeline/controlflowanalysis/expression_simplification/constant_folding.py
+++ decompiler/pipeline/controlflowanalysis/expression_simplification/constant_folding.py
@@ -109,7 +109,8 @@ def normalize_int(v: int, size: int, signed: bool) -> int:
 
     This function takes an integer value 'v' and normalizes it to fit within
     the specified 'size' in bits by discarding overflowing bits. If 'signed' is
-    true, the value is treated as a signed integer.
+    true, the value is treated as a signed integer, i.e. interpreted as a two's complement.
+    Therefore the return value will be negative iff 'signed' is true and the most-significant bit is set.
 
     :param v: The value to be normalized.
     :param size: The desired bit size for the normalized integer.
