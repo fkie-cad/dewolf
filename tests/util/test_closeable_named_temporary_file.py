fabb1ce32169d1510f8558aa1edbb983af660f71
Manuel Blatt <manuel.blatt@fkie.fraunhofer.de>
2023-12-07 13:09:42 +0100

    black

--- tests/util/test_closeable_named_temporary_file.py
+++ tests/util/test_closeable_named_temporary_file.py
@@ -1,26 +1,27 @@
 import os
+
 from decompiler.util.closeable_named_temporary_file import CloseableNamedTemporaryFile
 
 
 class TestCloseableNamedTemporaryFile:
     def test_usage_after_closing(self):
-        with CloseableNamedTemporaryFile(mode = "w") as file:
+        with CloseableNamedTemporaryFile(mode="w") as file:
             file.write("test")
             file.close()
             with open(file.name, "r") as reopened_file:
                 assert reopened_file.read() == "test"
 
     def test_deletion_with_close(self):
-        with CloseableNamedTemporaryFile(mode = "w") as file:
+        with CloseableNamedTemporaryFile(mode="w") as file:
             file.close()
         assert not os.path.exists(file.name)
 
     def test_deletion_without_close(self):
-        with CloseableNamedTemporaryFile(mode = "w") as file:
+        with CloseableNamedTemporaryFile(mode="w") as file:
             pass
         assert not os.path.exists(file.name)
 
     def test_close_after_delete(self):
-        with CloseableNamedTemporaryFile(mode = "w") as file:
+        with CloseableNamedTemporaryFile(mode="w") as file:
             pass
         file.close()
