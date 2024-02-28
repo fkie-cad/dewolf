cc20f664131faf3ed33e2b706b306fb23cc8fc74
rihi <19492038+rihi@users.noreply.github.com>
2024-02-28 14:09:05 +0100

    black

--- update_for_release.py
+++ update_for_release.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         data = json.load(file)
 
     # Assuming the version string follows the "major.minor.patch" format
-    version_parts = data['version'].split('.')
+    version_parts = data["version"].split(".")
     major, minor, patch = map(int, version_parts)
 
     # Increment the patch version
@@ -15,10 +15,10 @@ if __name__ == "__main__":
 
     # Update the version in the data
     new_version = f"{major}.{minor}.{patch}"
-    data['version'] = new_version
+    data["version"] = new_version
 
     with release_path.open("w") as file:
         json.dump(data, file, indent=4)
-    
+
     # Print new version so the github action can use it
     print(new_version)
