4de78d78b802bee775838940e98ded5e036af30e
rihi <19492038+rihi@users.noreply.github.com>
2024-02-29 13:02:02 +0100

    Update version string generation

--- update_for_release.py
+++ update_for_release.py
@@ -1,4 +1,5 @@
 import json
+import time
 from pathlib import Path
 
 if __name__ == "__main__":
@@ -6,15 +7,13 @@ if __name__ == "__main__":
     with release_path.open("r") as file:
         data = json.load(file)
 
-    # Assuming the version string follows the "major.minor.patch" format
-    version_parts = data["version"].split(".")
-    major, minor, patch = map(int, version_parts)
+    old_version: str = data["version"]
+    new_version = time.strftime("%Y-%m-%d")
 
-    # Increment the patch version
-    patch += 1
+    if old_version.startswith(new_version):
+        patch = int(old_version[index + 1:]) if (index := old_version.rfind(".")) != -1 else 0
+        new_version += f".{patch + 1}"
 
-    # Update the version in the data
-    new_version = f"{major}.{minor}.{patch}"
     data["version"] = new_version
 
     with release_path.open("w") as file:
