ad3019fcdcdfb63418c4dd96352e63498d0b5d33
rihi <19492038+rihi@users.noreply.github.com>
2024-03-04 16:45:09 +0100

    Fix automatic release
    Changes automatic release to not push release commits to any branch. Instead, the commit is simply tagged and only the tag is pushed. This results in the tags to not be included in any branch.

--- update_for_release.py
+++ update_for_release.py
@@ -1,18 +1,25 @@
 import json
+import subprocess
 import time
 from pathlib import Path
 
+
+def get_patch(line: str) -> int:
+    return int(line[index + 1 :]) if (index := line.rfind(".")) != -1 else 0
+
+
 if __name__ == "__main__":
+    new_version = time.strftime("%Y-%m-%d")
+
+    process = subprocess.run(["git", "tag", "-l", f"v{new_version}*"], text=True, capture_output=True)
+    patch = max((get_patch(line) for line in process.stdout.splitlines()), default=None)
+
+    if patch is not None:
+        new_version += f".{patch + 1}"
+
     release_path = Path("plugin.json")
     with release_path.open("r") as file:
         data = json.load(file)
-
-    old_version: str = data["version"]
-    new_version = time.strftime("%Y-%m-%d")
-
-    if old_version.startswith(new_version):
-        patch = int(old_version[index + 1 :]) if (index := old_version.rfind(".")) != -1 else 0
-        new_version += f".{patch + 1}"
 
     data["version"] = new_version
 
