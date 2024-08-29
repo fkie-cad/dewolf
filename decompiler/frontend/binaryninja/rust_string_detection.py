ce163e98c1cd547ea0b43385cdb03dc721c07fae
Manuel Blatt <manuel.blatt@fkie.fraunhofer.de>
2024-08-29 16:23:57 +0200

    polish string slicer integration

--- decompiler/frontend/binaryninja/rust_string_detection.py
+++ decompiler/frontend/binaryninja/rust_string_detection.py
@@ -4,20 +4,29 @@ import sys
 from binaryninja import BinaryView
 from decompiler.util.options import Options
 
-string_slicer_path = "/home/manuel/repos/"
-
 
 class RustStringDetection:
-    """TODO:"""
+    """
+    This 'stage' detects certain Rust strings (string slices), which are struct based strings.
+    It requires the RustStringSlicer. A path to the tool needs to be configured via the options.
+
+    The stage is executed before lifting, as it uses the Binary Ninja API to identify string slices
+    and 'mark' them, by assigning the appropriate type.
+    It can be configured to run always, never, or for Rust binaries only.
+    """
 
     def __init__(self, binary_view: BinaryView, options: Options):
         self._bv = binary_view
-        # TODO: add to default settings, change fallback
         self._enabled = options.getboolean("rust-string-detection.enabled", fallback=True)
         self._rust_binaries_only = options.getboolean("rust-string-detection.rust_binaries_only", fallback=True)
+        self._string_slicer_path = options.getstring("rust-string-detection.string_slicer_path")
         self._debug_submodules = options.getboolean("logging.debug-submodules")
 
     def is_rust_binary(self):
+        """
+        Simple heurstic to determine, whether the binary is a Rust binary.
+
+        """
         for _ in self._bv.find_all_data(self._bv.start, self._bv.end, "rustc".encode("utf-8")):
             return True
         for _ in self._bv.find_all_data(self._bv.start, self._bv.end, "cargo".encode("utf-8")):
@@ -26,7 +35,9 @@ class RustStringDetection:
 
     def run(self):
         """
-        TODO:
+        Runs the Rust String Slicer, if the required conditions are met.
+
+        String Slicer's path will be added to Python's path before importing the module.
         """
         if not self._enabled:
             return
@@ -37,7 +48,7 @@ class RustStringDetection:
 
         logging.info("Starting Rust String Slicer")
         try:
-            sys.path.append(string_slicer_path)
+            sys.path.append(self._string_slicer_path)
             from rust_string_slicer.binja_plugin.actions import RecoverStringFromReadOnlyDataTask, RustStringSlice
 
             if not RustStringSlice.check_binary_ninja_type_exists(self._bv):
