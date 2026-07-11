722f780a5587d60f551ece9b2fa204066d970b80
Steffen Enders <steffen.enders@yahoo.de>
2026-07-11 13:26:45 +0200

    Fix plugin test for Binary Ninja 5 API removal

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,4 +1,4 @@
-from binaryninja import BinaryViewType
+from binaryninja import load
 
 from __init__ import decompile
 
@@ -6,6 +6,6 @@ from __init__ import decompile
 def test_plugin():
     """Test if plugin does not raise errors"""
     filename = "tests/samples/bin/systemtests/64/0/test_loop"
-    bv = BinaryViewType.get_view_of_file_with_options(filename)
+    bv = load(filename)
     function = bv.get_functions_by_name("main")[0]
     decompile(bv, function)
